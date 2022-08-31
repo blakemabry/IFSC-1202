13a105d7ab601aef09325168ef38dc49922ed30f
blakemabry <wbmabry@ualr.edu>
2022-08-31 18:06:43 +0000

    School desks

---
+++ 01.07  School Desks.py
@@ -0,0 +1,13 @@
+a = input("Enter Classroom A: ")
+b = input("Enter Classroom B: ")
+c = input("Enter Classroom C: ")
+x = 2
+y = 1
+d = (int(a) // x)
+e = (int(b) // x)
+f = (int(c) // x)
+g =(int(d) % y)
+h = (int(e) % y)
+i = (int(f) % y)
+j = (g + h + i)
+print(j)
