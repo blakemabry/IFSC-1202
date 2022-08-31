8de6f29a8ce1d90ccf9a429c8cc6bcf00e15cbcf
blakemabry <wbmabry@ualr.edu>
2022-08-31 17:42:34 +0000

    Previous and Next Numbers

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,5 @@
+def printPreviousAndNext():
+  take = int(input())
+  print(f"The next number for the number {take} is {take+1}")
+  print(f"The previous number for the number {take} is {take-1}")
+printPreviousAndNext()
